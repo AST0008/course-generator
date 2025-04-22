bf09b4b1e00723a098cf98b8dbc43c6b1eff0361
unknown <ashwajittayade30@gmail.com>
2025-04-22 23:54:33 +0530

    added quiz cards

--- app/course/[...slug]/page.tsx
+++ app/course/[...slug]/page.tsx
@@ -2,7 +2,9 @@ import { prisma } from "@/lib/db";
 import { redirect } from "next/navigation";
 import CourseSideBar from "@/components/CourseSideBar";
 import MainVideoSummary from "@/components/MainVideoSummary";
-
+import QuizCards from "@/components/QuizCards";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import Link from "next/link";
 type Props = {
   params: {
     slug: string[];
@@ -10,7 +12,7 @@ type Props = {
 };
 
 const CoursePage = async ({ params }: Props) => {
-  const { slug } = params;
+  const { slug } = await params;
   const [courseId, UnitIndexParam, ChapterIndexParam] = slug;
   const course = await prisma.course.findUnique({
     where: {
@@ -44,16 +46,59 @@ const CoursePage = async ({ params }: Props) => {
   if (!chapter) {
     return redirect("/gallery");
   }
-  return(
+  const nextChapter = unit.Chapter[chapterIndex + 1];
+  const previousChapter = unit.Chapter[chapterIndex - 1];
+  return (
     <div>
-
-        <CourseSideBar course={course} currentChapter = {chapter.id} />
-    <div className="ml-[400px] px-8">
-     <div className="flex">
-        <MainVideoSummary chapterIndex={chapterIndex} unit={unit} chapter={chapter} unitIndex={unitIndex} />
-        {/* <QuizCards chapter={chapter} /> */}
-     </div>
-    </div>
+      <CourseSideBar course={course} currentChapter={chapter.id} />
+      <div className="ml-[400px] px-8">
+        <div className="flex">
+          <MainVideoSummary
+            chapterIndex={chapterIndex}
+            unit={unit}
+            chapter={chapter}
+            unitIndex={unitIndex}
+          />
+          <QuizCards chapter={chapter} />
+        </div>
+        <div className="flex-1 h-[1px] text-gray-500 bg-gray-500"></div>
+        <div className="flex pb-8">
+          {previousChapter && (
+            <Link
+              href={`/course/${courseId}/${unitIndex}/${chapterIndex - 1}`}
+              className="flex mt-4 mr-auto w-fit hover:opacity-75"
+            >
+              <div className="flex items-center">
+                <ChevronLeft className="w-4 h-4" />
+                <div className="flex flex-col items-start ml-2">
+                  <span className="text-sm text-secondary-foreground/80">
+                    Previous
+                  </span>
+                  <span className="text-xl font-bold">
+                    {previousChapter.name}
+                  </span>
+                </div>
+              </div>
+            </Link>
+          )}
+          {nextChapter && (
+            <Link
+              href={`/course/${courseId}/${unitIndex}/${chapterIndex + 1}`}
+              className="flex mt-4 ml-auto w-fit hover:opacity-75"
+            >
+              <div className="flex items-center">
+                <div className="flex flex-col items-end mr-2">
+                  <span className="text-sm text-secondary-foreground/80">
+                    Next
+                  </span>
+                  <span className="text-xl font-bold">{nextChapter.name}</span>
+                </div>
+                <ChevronRight className="w-4 h-4" />
+              </div>
+            </Link>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ components/QuizCards.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { Chapter, Question } from "@prisma/client";
+import { cn } from "@/lib/utils";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Label } from "@/components/ui/label";
+import { useState, useCallback } from "react";
+import { Button } from "./ui/button";
+import { ChevronRight } from "lucide-react";
+import React from "react";
+type Props = {
+  chapter: Chapter & {
+    questions: Question[];
+  };
+};
+
+const QuizCards = ({ chapter }: Props) => {
+  const [answer, setAnswer] = useState<Record<string, string>>({});
+  console.log("answer", answer);
+  const [questionState, setQuestionState] = useState<
+    Record<string, boolean | null>
+  >({});
+
+  const checkAnswers = React.useCallback(() => {
+    const newQuestionState: Record<string, boolean | null> = {};
+    chapter.questions.forEach((question) => {
+      const userAnswer = answer[question.id];
+      if (!userAnswer) {
+        return;
+      }
+      if (userAnswer === question.answer) {
+        newQuestionState[question.id] = true;
+      } else {
+        newQuestionState[question.id] = false;
+      }
+    });
+    setQuestionState(newQuestionState);
+  }, [answer, chapter.questions, questionState]);
+
+  return (
+    <div className="flex-[1] mt-10 ml-8  ">
+      <h1 className="text-2xl font-bold">Quiz Cards</h1>
+      <div className="mt-2">
+        {chapter.questions.map((question) => {
+          return (
+            <div
+              key={question.id}
+              className={cn(
+                "border-secondary rounded-lg p-3 mb-4",
+                questionState[question.id] === true && "bg-green-700",
+                questionState[question.id] === false && "bg-red-700",
+                questionState[question.id] === null && "bg-secondary"
+              )}
+            >
+              <h1 className="text-lg font-semibold">{question.question}</h1>
+              <div className="mt-2">
+                <RadioGroup
+                  onValueChange={(value) =>
+                    setAnswer((prev) => ({ ...prev, [question.id]: value }))
+                  }
+                >
+                  {JSON.parse(question.options).map(
+                    (option: string, index: number) => (
+                      <div key={option} className="flex items-center space-x-2">
+                        <RadioGroupItem
+                          value={option}
+                          id={question.id + index.toString()}
+                        />
+                        <Label htmlFor={question.id + index.toString()}>
+                          {option}
+                        </Label>
+                      </div>
+                    )
+                  )}
+                </RadioGroup>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <Button className="mt-2 w-full" size="lg" onClick={checkAnswers}>
+        Check Answers
+        <ChevronRight className="w-4 h-4 ml-2" />
+      </Button>
+    </div>
+  );
+};
+
+export default QuizCards;
